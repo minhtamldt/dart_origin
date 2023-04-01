1996b446b1fa0df75a3d5930da22d9b3224eb294
TamTM <minhtamldt@gmail.com>
2023-04-01 19:40:49 +0700

    save demo stream periodic

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,27 @@
+import 'dart:isolate';
+import 'dart:math';
+
+import 'package:test/expect.dart';
+
 void main(List<String> arguments) {
-  print('Hello world: ');
+  demoPeriodic();
+}
+
+Duration _duration = Duration(seconds: 2);
+void demoPeriodic() {
+
+  //periodic : Phương thức periodic hỗ trợ cứ mỗi Duration sẽ gửi data đi.
+  //listen: Phương thức để nhận data mà periodic đã gửi đi.
+  print("init Stream Periodic");
+  var stream = Stream<int>.periodic(_duration, (int count) => sentData());
+  print("listen Stream Periodic");
+  stream.listen(receiveData);
+}
+
+int sentData() {
+  return Random().nextInt(100);
+}
+
+void receiveData(int data) {
+  print("receiveData: ${data}");
 }
